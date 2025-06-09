335c801224fbacf873e260c5f3dadc07fab2baed
Peter Igaga <igagapeter477@gmail.com>
2025-06-09 01:02:58 +0100

    added facilities and comments

--- models/facilityData.js
+++ models/facilityData.js
@@ -3,21 +3,37 @@ import mongoose from "mongoose";
 const Schema = mongoose.Schema;
 
 const FacilitySchema = new Schema({
-  numClassrooms: String,
-  classroomOkay: String,
-  venCon: String,
-  numTables: String,
-  numChairs: String,
-  furnitureOkay: Number,
-  toiletOkay: String,
-  comment: String,
-  functionalLib: String,
-  wasteDisposal: Number,
-  boreHoles: String,
-  backupPower: String,
-  fencing: String,
-  teachingAid: String,
-  learningMat: String,
+  blocksOfClassrooms: Number,
+  numOfClassrooms: {
+    pupils: { value: Number, comment: String },
+    students: { value: Number, comment: String },
+    eccde: { value: Number, comment: String },
+  },
+  venOrConducive: {
+    pupils: { value: String, comment: String },
+    students: { value: String, comment: String },
+    eccde: { value: String, comment: String },
+  },
+  numOfFurnitures: {
+    pupils: { value: Number, comment: String },
+    students: { value: Number, comment: String },
+    eccde: { value: Number, comment: String },
+    teachers: { value: Number, comment: String },
+  },
+  toiletFacility: {
+    pupils: { value: String, comment: String },
+    students: { value: String, comment: String },
+    eccde: { value: String, comment: String },
+  },
+  fencing: { value: String, comment: String },
+  teachingAid: {
+    blackboard: { value: String, comment: String },
+    textbook: { value: String, comment: String },
+    whiteboard: { value: String, comment: String },
+    lessonNotes: { value: String, comment: String },
+  },
+  agricFarm: { value: String, comment: String },
+  sportFacility: { value: String, comment: String },
 });
 
 const FacilityData = mongoose.model("FacilityData", FacilitySchema);
